484ad26f7ae95073b7127866ec1e7902884bf0af
ViniWei <viniciusweigert152@gmail.com>
2025-08-10 14:30:07 -0300

    Make tmux start on startup

--- shell/shell/bashrc_custom.sh
+++ shell/shell/bashrc_custom.sh
@@ -2,6 +2,10 @@ function p() {
     cd -- "$(. ~/shell/project_finder.sh)" 
 }
 
+if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
+    exec tmux new-session -A -s ${USER} >/dev/null 2>&1
+fi
+
 alias n="nvim"
 
 set -o vi
