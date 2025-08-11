891d64a3dbcd8bb4a22ca0ca26a0e38fd5928f22
Vinicius Weigert <vweigert@gocn.com.br>
2025-08-11 11:21:03 -0300

    Make tmux not start on startup

--- shell/shell/bashrc_custom.sh
+++ shell/shell/bashrc_custom.sh
@@ -2,10 +2,6 @@ function p() {
     cd -- "$(. ~/shell/project_finder.sh)" 
 }
 
-if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
-    exec tmux new-session -A -s ${USER} >/dev/null 2>&1
-fi
-
 alias n="nvim"
 
 set -o vi
