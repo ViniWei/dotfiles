d3ad197b23374bf06b37f581ab1db662ba83ae7a
ViniWei <viniciusweigert152@gmai.com>
2026-02-19 21:10:35 -0300

    separeted file for ubuntu setup installs

--- shell/shell/setup.sh
+++ shell/shell/setup.sh
@@ -7,15 +7,4 @@ if ! grep -qF "$setup_string" $bashrc; then
     echo "$setup_string" >> $bashrc
 fi
 
-sudo apt update
-sudo apt install stow
-sudo apt install clang
-sudo apt install fzf
-sudo apt install xclip
-sudo apt install tmux
-sudo apt install ripgrep
-sudo snap install --classic nvim
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-curl -sS https://starship.rs/install.sh | sh
-
 . $bashrc
